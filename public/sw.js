63562252312c477b6f3a6e4141ecb6998affafb6
SQasimRizvi <syed.rizvi2385@gmail.com>
2018-08-14 02:37:19 +0500

    trimming the cache upto 10 dynamic files #8

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,34 @@
 const CACHE_STATIC_NAME = 'static-v13';
 const CACHE_DYNAMIC_NAME = 'dynamic-v2';
 const STATIC_FILES = [
-					'/',
-					'/index.html',
-					'/offline.html',
-					'/src/js/app.js',
-					'/src/js/feed.js',
-					'/src/js/promise.js',
-					'/src/js/fetch.js',
-					'/src/js/material.min.js',
-					'/src/css/app.css',
-					'/src/css/feed.css',
-					'/src/images/main-image.jpg',
-					'https://fonts.googleapis.com/css?family=Roboto:400,700',
-					'https://fonts.googleapis.com/icon?family=Material+Icons',
-					'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-				];
+	'/',
+	'/index.html',
+	'/offline.html',
+	'/src/js/app.js',
+	'/src/js/feed.js',
+	'/src/js/promise.js',
+	'/src/js/fetch.js',
+	'/src/js/material.min.js',
+	'/src/css/app.css',
+	'/src/css/feed.css',
+	'/src/images/main-image.jpg',
+	'https://fonts.googleapis.com/css?family=Roboto:400,700',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+];
+
+function trimCache(cacheName, maxItems) {
+	caches.open(cacheName)
+		.then(function(cache) {
+			return cache.keys()
+				.then(function(keys) {
+					if(keys.length > maxItems) {
+						cache.delete(keys[0])
+							.then(trimCache(cacheName, maxItems));
+					}
+				})
+		});
+}
 
 self.addEventListener('install', function(event) {
 	console.log('[Service Worker] Installing Service Worker ...', event);
@@ -64,6 +77,7 @@ self.addEventListener('fetch', function(event) {
 				.then(function(cache) {
 					return fetch(event.request)
 						.then(function(res) {
+							trimCache(CACHE_DYNAMIC_NAME, 10);
 							cache.put(event.request, res.clone());
 							return res;
 						});
@@ -84,6 +98,7 @@ self.addEventListener('fetch', function(event) {
 							.then(function(res) {
 								return caches.open(CACHE_DYNAMIC_NAME)
 									.then(function(cache) {
+										trimCache(CACHE_DYNAMIC_NAME, 10);
 										cache.put(event.request.url, res.clone());
 										return res;
 									})
